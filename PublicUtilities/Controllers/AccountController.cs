523f186c5a42d51a3671dd83284346fa2a339492
S0k1l <martynuky16@gmail.com>
2023-12-21 23:00:31 +0200

    Add project files.

---
+++ PublicUtilities/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PublicUtilities.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Registration()
+        {
+            return View();
+        }
+    }
+}
---
+++ PublicUtilities/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PublicUtilities.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
