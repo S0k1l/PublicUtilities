631c1e73b49fe365fe4d79d2e8daee1c03424423
S0k1l <martynuky16@gmail.com>
2023-12-25 20:57:54 +0200

    Made Payment page front-end v1

---
+++ PublicUtilities/Controllers/PaymentController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PublicUtilities.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Water()
+        {
+            return View();
+        }
+        public IActionResult Gas()
+        {
+            return View();
+        }
+        public IActionResult Electricity()
+        {
+            return View();
+        }
+    }
+}
