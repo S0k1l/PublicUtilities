8c4106384b127d1784f3499abd6985f6145a083c
S0k1l <martynuky16@gmail.com>
2023-12-31 21:06:31 +0200

    Made GarbageRemoval page front-end

---
+++ PublicUtilities/Controllers/GarbageRemovalController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PublicUtilities.Controllers
+{
+    public class GarbageRemovalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
