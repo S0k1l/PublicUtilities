e02f96554a620200a111cfcf21611c92a1e03c45
S0k1l <martynuky16@gmail.com>
2024-01-01 16:36:41 +0200

    Made Tariffs page front-end

---
+++ PublicUtilities/Controllers/TariffsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PublicUtilities.Controllers
+{
+    public class TariffsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
