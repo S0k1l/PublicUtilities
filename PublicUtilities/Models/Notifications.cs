d3bc64c979ee9e0c65eea60ec922337aa7a77c62
S0k1l <martynuky16@gmail.com>
2024-04-11 19:57:15 +0300

    Update Notification model

--- PublicUtilities/Models/Notifications.cs
+++ PublicUtilities/Models/Notifications.cs
@@ -6,9 +6,9 @@ namespace PublicUtilities.Models
     public class Notifications
     {
         public int Id { get; set; }
-        public int PlacesOfResidenceId { get; set; }
+        public int? PlacesOfResidenceId { get; set; }
         [ForeignKey("PlacesOfResidenceId")]
-        public PlacesOfResidence PlacesOfResidence { get; set; }
+        public PlacesOfResidence? PlacesOfResidence { get; set; }
         public string Header { get; set; }
         public string Text { get; set; }
         public DateTime Date { get; set; }
